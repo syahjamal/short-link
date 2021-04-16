fb62df3d139d95e5027b932fcd2ac397d16a2589
syahjamal <sulaimansyahjamal@gmail.com>
2021-04-16 14:05:23 +0700

    History Page

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Container from '../components/Container';
+import Col from '../components/Col';
+import Card from '../components/Card';
+import Row from '../components/Row';
 
 function History(props) {
     return (
         <>
-            <h1>History Page</h1>
+            <Container className="mt-5">
+                <Row>
+                    <Col className="col-md-10 offset-md-1">
+                        <h1>Histories</h1>
+                        <Card className="card">    
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Original URL</th>
+                                        <th>Short URL</th>
+                                        <th>Alternative URL</th>
+                                        <th>QR</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>XXX</td>
+                                        <td>XXX</td>
+                                        <td>XXX</td>
+                                        <td>XXX</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,21 +17,51 @@ function Home(props) {
                                 <img
                                     src="https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/qr_code-256.png"
                                     width="140"
-                                    alt="logo" 
+                                    alt="logo"
                                 />
-                                <br/>
+                                <br />
                                 <small>
                                     QR Code and URL Shortener Generator
                                 </small>
                             </div>
                             <form className="mt-2">
-                                <div className="form-group" >
-                                    <input type="text" className="form-control search__input"/>
+                                <div className="form-group mt-3" >
+                                    <input type="text" className="form-control search__input" />
                                 </div>
-                                <div className="form-group" style={{padding: '24px 48px 24px 48px'}}>
+                                <div className="form-group" style={{ padding: '24px 48px 24px 48px' }}>
                                     <button type="button" className="btn btn-primary w-100">Generate</button>
                                 </div>
                             </form>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="mt-5 mb-5">
+                <Row>
+                    <Col className="col-md-6 offset-md-3">
+                        <Card className="card md-3">
+                            <div className="text-center mt-3">
+                                <small>
+                                    Result
+                                </small>
+                                <br/>
+                                <img
+                                    src="https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/qr_code-256.png"
+                                    width="140"
+                                    alt="logo"
+                                />
+                            </div>
+                            <ul className="list-group mt-3" style={{fontSize:"small", padding: '0px 24px 24px 24px'}}>
+                                <li className="list-group-item">
+                                    Short URL: <a href="/">XXX</a>
+                                </li>
+                                <li className="list-group-item">
+                                    Alternative URL : <a href="/">XXX</a>
+                                </li>
+                                <li className="list-group-item">
+                                    Original URL : <a href="/">XXX</a>
+                                </li>
+                            </ul>
                         </Card>
                     </Col>
                 </Row>
